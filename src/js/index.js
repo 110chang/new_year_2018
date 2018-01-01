618bf9177f04e016294bcd3cbff45cfa88a794b4
110chang <u@110chang.com>
2018-01-01 13:35:06 +0900

    Extend duration of mask

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,6 @@
 import './tracking-min';
 import './face-min';
 
-window.URL = window.URL || window.webkitURL;
-
 !function() {
 
   const DPR = window.devicePixelRatio;
@@ -15,10 +13,13 @@ window.URL = window.URL || window.webkitURL;
   const play = document.getElementById('btn-play');
   const stop = document.getElementById('btn-stop');
   const tracker = new tracking.ObjectTracker(['face']);
+  const MAX_COUNT = 10;
 
   let contentWidth = window.innerWidth;
   let contentHeight = window.innerHeight;
   let track;
+  let memTrackers = [];
+  let untrackCount = 0;
 
   function initialize() {
     img.src = '../img/Laughing_man.png';
@@ -74,15 +75,27 @@ window.URL = window.URL || window.webkitURL;
 
   tracker.on('track', e => {
 
+    let trackers = e.data;
+
     ctx.clearRect(0, 0, contentWidth * DPR, contentHeight * DPR);
 
-    if (e.data.length === 0) {
+    if (e.data == null || e.data.length === 0) {
       // No objects were detected in this frame.
-    } else {
-      console.log('detected', ctx.canvas.clientWidth, ctx.canvas.clientHeight);
-      e.data.forEach(rect => {
+      // console.log('untrack', untrackCount);
+      if (untrackCount > MAX_COUNT) {
+        trackers = [];
+        memTrackers = [];
+        untrackCount = 0;
+      } else {
+        trackers = memTrackers;
+        untrackCount++;
+      }
+    }
+    if (trackers.length > 0) {
+      // console.log('detected', ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+      trackers.forEach(rect => {
         let width, height;
-        console.log(rect);
+        // console.log(rect);
         if (rect.width > rect.height) {
           width = rect.width;
           height = rect.width * img.height / img.width;
@@ -94,11 +107,11 @@ window.URL = window.URL || window.webkitURL;
         // ctx.strokeRect(rect.x * DPR, rect.y * DPR, rect.width * DPR, rect.height * DPR);
         ctx.drawImage(img, rect.x * DPR, rect.y * DPR, width * DPR, height * DPR);
       });
+      memTrackers = trackers;
     }
   });
 
   initialize();
-
 
   tracking.track('#myVideo', tracker);
 
